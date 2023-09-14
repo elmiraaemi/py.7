642798276e9d8e35075183b8e93528661dcd560c
elmiraaemi <108354246+elmiraaemi@users.noreply.github.com>
2023-09-14 22:25:25 +0330

    my store.py

--- my store.py
+++ my store.py
@@ -1,6 +1,6 @@
 import pyfiglet
 import qrcode
-file = open('E://hdd/my websayt/python/t6/test.txt', 'r')
+file = open('E:\.vscode\web di\python\T7\stor .txt', 'r')
 data = file.read()
 product_list = data.split('\n')
 products = []
@@ -8,13 +8,25 @@ for i in range(len(product_list)):
     product_list_info = product_list[i].split(',')
     mydict =   {'id': int(product_list_info[0]),
                 'name': product_list_info[1],
-                'price': float(product_list_info[2]),
+                'price': int(product_list_info[2]),
                 'count': int(product_list_info[3]) }
     products.append(mydict)
 file.close()
+
 def loading():
     result = pyfiglet.figlet_format('MY STORE', font = 'roman')
     print(result)
+
+def menu_productes():
+    print('1 for add product')
+    print('2 for edit product')
+    print('3 for delete product')
+    print('4 for search product')
+    print('5 for show list productes')
+    print('6 for buy product')
+    print('7 for Qr code of product')
+    print('8 for exit')
+
 def add_productes():
     c=int(input('id ? : '))
     n=input('name ? : ')
@@ -22,31 +34,31 @@ def add_productes():
     k=int(input('count ? : '))
     products.append({'id':c,'name':n,'price':p,'count':k})
     print('Done')
-def show_edit():
-    print('1 for name')
-    print('2 for price')
-    print('3 for count')
-    print('4 for exit')
+
 def edit_productes():
     na=input('name or id of product ? : ')
     for i in range(len(products)):
         if products[i]['name'] == na or str(products[i]['id'] )== na:
             while True:
-                    show_edit()
-                    e = int(input())
-                    if e == 1:
-                        products[i]['name']=input(' type rename product : ')
-                        print('Done')
-                    elif e ==2:
-                        products[i]['price']=float(input(' type reprice product : '))
-                        print('Done')
-                    elif e == 3:
-                        products[i]['count']=int(input(' type recount product : '))
-                        print('Done')
-                    elif e ==4:
-                        break  
-                    else:
-                        print('Does not exist')
+                print('1 for name')
+                print('2 for price')
+                print('3 for count')
+                print('4 for exit')
+                e = int(input())
+                if e == 1:
+                    products[i]['name']=input(' type rename product : ')
+                    print('Done')
+                elif e ==2:
+                    products[i]['price']=float(input(' type reprice product : '))
+                    print('Done')
+                elif e == 3:
+                    products[i]['count']=int(input(' type recount product : '))
+                    print('Done')
+                elif e ==4:
+                    break  
+                else:
+                    print('Does not exist')
+
 def delete_productes():
     na=input('name or id of product ? : ')
     for i in range(len(products)):
@@ -56,6 +68,7 @@ def delete_productes():
             break
     else:
         print('Does not exist')
+
 def search_productes():
     na=input('name or id of product ? : ')
     for i in range(len(products)):
@@ -64,22 +77,28 @@ def search_productes():
             break
     else:
         print('Does not exist')
+
 def show_productes():
     for i in range(len(products)):
         print(products[i])
+
 def Qr_productes():
-    na=input('name or id of product ? : ')
+    name=input('name or id of product ? : ')
     for i in range(len(products)):
-        if products[i]['name'] == na or str(products[i]['id'] )== na:
-          qr=qrcode.make(products[i])
-          qr.save(f'qrcode{i}.png')
+        if products[i]['name'] == name or str(products[i]['id'] )== name:
+          QR=[]
+          QR.append(products[i]['name'])
+          QR.append(products[i]['id'])
+          img=qrcode.make(QR)
+          img.save("qrcode.jpg")
           print('Done')
+
 def buy_productes():
     pri=[]
     while True:
-        na=input('name or id of product ? : ')
+        name=input('name or id of product ? : ')
         for i in range(len(product_list)):
-            if products[i]['name'] == na or str(products[i]['id'] )== na:
+            if products[i]['name'] == name or str(products[i]['id'] )== name:
                 cou=int(input('count ? : '))
                 if int(products[i]['count'])>=cou:
                     pri.append({'name':products[i]['name'],
@@ -100,25 +119,20 @@ def buy_productes():
             tpri+=pri[i]['price']*pri[i]['count']
         print('total price : ', tpri)
         break
+
 def exit_store():
-    f=open('E://hdd/my websayt/python/t6/test.txt','w')
+    f=open('E:\.vscode\web di\python\T7\stor .txt','w')
     for i in range(len(products)):
         data=str(products[i]['id'])+','+products[i]['name']+','+str(products[i]['price'])+','+str(products[i]['count'])+'\n'  
         f.write(data)
     f.close()
     exit()
-def menu_productes():
-    print('1 for add product')
-    print('2 for edit product')
-    print('3 for delete product')
-    print('4 for search product')
-    print('5 for show list productes')
-    print('6 for buy product')
-    print('7 for Qr code of product')
-    print('8 for exit')
+
+
+
 loading()
 while True:
-    menu_productes()
+        menu_productes()
     a=int(input())
     if   a<2:
         add_productes()
